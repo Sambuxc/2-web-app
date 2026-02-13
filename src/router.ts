21eb621c95c7228ba3e44dcefe8787c4c2ea71d6
Sambuxc <samwbru@gmail.com>
2026-02-13 08:44:48 +0000

    - converts home view to vue3 file

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import { HomeView } from './views/HomeView.ts';
+import HomeView from './views/HomeView.vue';
 import { RewardsView } from './views/RewardsView.ts';
-import { MoreView } from './views/MoreView.ts';
+import MoreView from './views/MoreView.vue';
 import { HowItWorksView } from './views/HowItWorksView.ts';
 
 export const routes = [
--- src/views/MoreView.vue
+++ src/views/MoreView.vue
@@ -1,10 +1,14 @@
+<script setup lang="ts">
 const items = ['messages', 'find your wagamama', 'refer a friend', 'gift cards', 'claim a stamp', 'contact us'];
+// data: () => ({ items })
 
-export const MoreView = {
-  template: `
+console.log(' more view')
+</script>
+
+<template>
     <section class="page scroll-page">
       <h2 class="page-title">more</h2>
-      <img src="./public/assets/more-hero.svg" alt="friends dining" class="hero" />
+      <img src="../../public/assets/more-hero.svg" alt="friends dining" class="hero" />
       <div class="menu-list">
         <button v-for="item in items" :key="item" class="menu-item">
           <span>{{ item }}</span>
@@ -12,6 +16,4 @@ export const MoreView = {
         </button>
       </div>
     </section>
-  `,
-  data: () => ({ items }),
-};
+</template>
